abe85c913ec27815cd56768809189778d9226c67
Romain Roffé <rofferom@gmail.com>
2021-12-26 10:51:03 +0100

    Add day21 part2

--- src/day21/main.rs
+++ src/day21/main.rs
@@ -1,27 +1,49 @@
 use std::cmp::min;
+use std::collections::HashMap;
 use std::fs;
 
 const INPUT_PATH: &str = "src/day21/input.txt";
 
-#[derive(Debug)]
+#[derive(Clone, Copy, Debug)]
 struct Player {
-    pos: usize,
-    score: usize,
+    pos: u64,
+    score: u64,
 }
 
-fn part1(input: &str) -> usize {
+impl Player {
+    fn new(pos: u64) -> Self {
+        Self { pos, score: 0 }
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Universe {
+    players: [Player; 2],
+    player_idx: usize,
+    rolls: u64,
+}
+
+impl Universe {
+    fn new(players: [Player; 2]) -> Self {
+        Self {
+            players,
+            player_idx: 0,
+            rolls: 1,
+        }
+    }
+}
+
+fn parse_players(input: &str) -> [Player; 2] {
     let lines: Vec<_> = input.lines().collect();
 
-    let mut players = vec![
-        Player {
-            pos: lines[0][28..].parse::<usize>().unwrap() - 1,
-            score: 0,
-        },
-        Player {
-            pos: lines[1][28..].parse::<usize>().unwrap() - 1,
-            score: 0,
-        },
-    ];
+    [
+        Player::new(lines[0][28..].parse::<u64>().unwrap() - 1),
+        Player::new(lines[1][28..].parse::<u64>().unwrap() - 1),
+    ]
+}
+
+pub fn part1(input: &str) -> u64 {
+    let mut players = parse_players(input);
 
     let mut player_idx = 0;
     let mut die_rolled = 0;
@@ -29,13 +51,10 @@ fn part1(input: &str) -> usize {
     while players[0].score < 1000 && players[1].score < 1000 {
         let mut distance = 0;
 
-        for _ in 0..3 {
-            die_rolled += 1;
-            distance += die_rolled;
-        }
+        distance += die_rolled * 3 + 6;
+        die_rolled += 3;
 
         let mut player = &mut players[player_idx];
-
         player.pos = (player.pos + distance) % 10;
         player.score += player.pos + 1;
 
@@ -45,9 +64,57 @@ fn part1(input: &str) -> usize {
     min(players[0].score, players[1].score) * die_rolled
 }
 
+fn compute_rolls() -> Vec<(u64, u64)> {
+    let mut rolls = HashMap::new();
+
+    for i in 1..4 {
+        for j in 1..4 {
+            for k in 1..4 {
+                let r = i + j + k;
+                let e = rolls.entry(r).or_insert(0);
+
+                *e += 1;
+            }
+        }
+    }
+
+    let mut v: Vec<(u64, u64)> = rolls.iter().map(|(&k, &v)| (k, v)).collect();
+    v.sort_by(|(a, _), (b, _)| a.partial_cmp(b).unwrap());
+
+    v
+}
+
+pub fn part2(input: &str) -> u64 {
+    let rolls = compute_rolls();
+
+    let mut universes = vec![Universe::new(parse_players(input))];
+    let mut player_wins = vec![0; 2];
+
+    while let Some(universe) = universes.pop() {
+        for (distance, count) in &rolls {
+            let mut new_universe = universe;
+            new_universe.rolls *= count;
+
+            let player = &mut new_universe.players[universe.player_idx];
+            player.pos = (player.pos + distance) % 10;
+            player.score += player.pos + 1;
+
+            if player.score >= 21 {
+                player_wins[universe.player_idx] += new_universe.rolls;
+            } else {
+                new_universe.player_idx = (new_universe.player_idx + 1) % 2;
+                universes.push(new_universe);
+            }
+        }
+    }
+
+    *player_wins.iter().max().unwrap()
+}
+
 fn main() {
     let input = fs::read_to_string(INPUT_PATH).unwrap();
     println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
 }
 
 #[cfg(test)]
@@ -61,5 +128,14 @@ Player 2 starting position: 8";
     fn day21_part1() {
         assert_eq!(part1(INPUT), 739785);
         assert_eq!(part1(&fs::read_to_string(INPUT_PATH).unwrap()), 797160);
+    }
+
+    #[test]
+    fn day21_part2() {
+        assert_eq!(part2(INPUT), 444356092776315);
+        assert_eq!(
+            part2(&fs::read_to_string(INPUT_PATH).unwrap()),
+            27464148626406
+        );
     }
 }
